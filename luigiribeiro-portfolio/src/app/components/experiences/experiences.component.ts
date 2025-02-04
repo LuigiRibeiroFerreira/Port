c1993d3298fd95395ef858b69f250b0821781140
Luigi Ribeiro Ferreira <Luigi_ribeiro@live.com>
2025-02-04 18:03:49 -0300

    refactor(about): split experiences into reusable components(incomplete

--- luigiribeiro-portfolio/src/app/components/experiences/experiences.component.ts
+++ luigiribeiro-portfolio/src/app/components/experiences/experiences.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit, ElementRef, AfterViewInit  } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, Input} from '@angular/core';
 import AOS from 'aos';
 import VanillaTilt from 'vanilla-tilt';
-
+import { CardData } from '../../models/card.model';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-experiences',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent],
   templateUrl: './experiences.component.html',
   styleUrl: './experiences.component.css'
 })
 export class ExperiencesComponent {
+  @Input() experiences!: CardData[];
   ngOnInit() {
     AOS.init();
   }
---
+++ luigiribeiro-portfolio/src/app/models/card.model.ts
@@ -0,0 +1,14 @@
+export interface CardData {
+    type: 'experience' | 'project' | 'blog'; // Discriminator for styling/logic
+    title: string;
+    subtitle?: string; // Company (experience) or Institution (project)
+    description: string;
+    imagePath: string;
+    // Project-specific fields
+    technologies?: string[];
+    projectLink?: string;
+    // Blog-specific fields
+    articleLink?: string;
+    publishDate?: Date;
+    tags?: string[];
+}
