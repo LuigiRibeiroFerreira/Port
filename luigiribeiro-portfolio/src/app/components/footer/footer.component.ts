4a6bd91fdcb2949c9d726358a5e3385d477ccf5e
Luigi Ribeiro Ferreira <Luigi_ribeiro@live.com>
2025-02-14 16:12:00 -0300

    feat(footer): add flip animation to language switch button

--- luigiribeiro-portfolio/src/app/components/footer/footer.component.ts
+++ luigiribeiro-portfolio/src/app/components/footer/footer.component.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
+  
 })
 export class FooterComponent {
+  isEnglish = false;
+  isFlipping = false;
 
+  toggleLanguage() {
+    this.isFlipping = true;
+
+    setTimeout(() => {
+      this.isEnglish = !this.isEnglish;
+    }, 300); 
+
+    setTimeout(() => {
+      this.isFlipping = false;
+    }, 600); 
+  }
 }
